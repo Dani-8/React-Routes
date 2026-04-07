ef22a068a775d7a95d359caf0e5a9f549df36b54
dani-8 <daniyalw6190@gmail.com>
2026-04-07 23:25:37 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Dashboard from './pages/Dashboard';
+import ProfilesOverview, { profilesLoader } from './pages/ProfilesOverview';
+import Profile, { profileLoader } from './pages/Profile';
+import NotFound from './pages/NotFound';
 
-import Navbar from './components/Navbar'
-
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-
-import Dashboard from './pages/Dashboard'
-import ProfilesOverview from './pages/ProfilesOverview'
-import Profile from './pages/Profile'
-
-import NotFound from './pages/NotFound'
-// -------------------------------------------------
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        <Route index element={<ProfilesOverview />} loader={profilesLoader} />
+        <Route path="profile/:id" element={<Profile />} loader={profileLoader} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />
-
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-
-
-        <Route path="/dashboard" element= {<Dashboard />}>
-          <Route index element={<ProfilesOverview />} />
-
-          <Route path="profile/:id" element={<Profile />} />
-        </Route>
-
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
